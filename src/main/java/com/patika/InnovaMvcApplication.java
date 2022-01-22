b0598ae641325166a17331a26164bbd29227f568
Beytullahzor7 <beytullahzor7@gmail.com>
2022-01-22 17:06:07 +0300

    Refactored

--- src/main/java/com/patika/InnovaMvcApplication.java
+++ src/main/java/com/patika/InnovaMvcApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class InnovaMvcApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(InnovaMvcApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(InnovaMvcApplication.class, args);
+	}
 
 }
