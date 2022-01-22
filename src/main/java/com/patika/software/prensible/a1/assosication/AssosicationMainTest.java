6df67e7af3491ec1716265783bd4cdb21066f69c
Beytullahzor7 <beytullahzor7@gmail.com>
2022-01-22 19:03:00 +0300

    Refactored

--- src/main/java/com/patika/software/prensible/a1/assosication/AssosicationMainTest.java
+++ src/main/java/com/patika/software/prensible/a1/assosication/AssosicationMainTest.java
@@ -5,9 +5,9 @@ import lombok.extern.log4j.Log4j2;
 @Log4j2
 public class AssosicationMainTest {
     public static void main(String[] args) {
-        Musteri musteri=Musteri.builder().musteriAdi("Hamit").musteriSehir("Malatya").build();
+        Musteri musteri=Musteri.builder().musteriAdi("Beytullah").musteriSehir("Samsun").build();
 
-        SuperMarket superMarket=SuperMarket.builder().superMarketAdi("Xyz44").superMarketSehir("Malatya").build();
+        SuperMarket superMarket=SuperMarket.builder().superMarketAdi("ABC").superMarketSehir("Samsun").build();
 
         //loose Coupling (Zayıf bağlantı türüne ==> Association diyoruz
         log.info("Müşteri adı "+musteri.getMusteriAdi()+" Süpermarket adi: "+superMarket.getSuperMarketAdi());
