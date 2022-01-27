23f8d4cdaff994521552a28c26fdf120e457c6f2
Beytullahzor7 <beytullahzor7@gmail.com>
2022-01-27 17:40:36 +0300

    ResponseBody

--- src/main/java/com/patika/web/_01_ResponseBody.java
+++ src/main/java/com/patika/web/_01_ResponseBody.java
@@ -5,6 +5,7 @@ package com.patika.web;
 
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -12,9 +13,35 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Log4j2
 public class _01_ResponseBody {
 
+    //1
+    // http://localhost:3333/birinci
     @GetMapping("/birinci")
     @ResponseBody //Herhangi bir yerde görünmeyip direkt olarak ekranda görünmesini istediğim için yazdım
     public String birinciMethod(){
         return "Yükleniyor...";
     }
+
+    //2
+    // http://localhost:3333/ikinci
+    @GetMapping("/ikinci")
+    public String ikinciMethod(){
+        return "index";
+    }
+
+    //3
+    // http://localhost:3333/ucuncu
+    @GetMapping("/ucuncu")
+    public String ucuncuMethod(){
+        return "other/deneme";
+    }
+
+    //4
+    // http://localhost:3333/dorduncu
+    @GetMapping("/dorduncu")
+    public String dorduncuMethod(Model model){ //key-value yapısı ile çalışır
+        model.addAttribute("param_key","Merhabalar Spring Model ile data geldi");
+        return "param";
+    }
+
+
 }
